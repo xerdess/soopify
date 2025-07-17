deb55b6b37112d69ae62a40f78068cc03c26cf4c
xerdes <ihavegotmew@gmail.com>
2025-07-18 03:06:47 +0530

    kuch toh kiya hu

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print ("hello world")
+from flask import Flask,redirect,render_template,request
+
+app= Flask(__name__)
+
+@app.route('/', methods=['GET','POST'])
+def fun():
+    if request.method == 'POST':
+        link = request.form.get('link')
+        return redirect(link)
+    return render_template('fun.html')
+
+if __name__=='__main__':
+    app.run(debug=True)
